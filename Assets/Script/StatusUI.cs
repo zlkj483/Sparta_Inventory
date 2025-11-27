26baffa0865c7d69f2b718c6aadddc6774ab29c9
Lim <mlimsg08@gmail.com>
2025-11-28 03:21:11 +0900

    feat: 스테이터스 업데이트기능 추가

--- Assets/Script/StatusUI.cs
+++ Assets/Script/StatusUI.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Data;
 
 public class StatusUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI attackText;
+    [SerializeField] private TextMeshProUGUI defenceText;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if(PlayerManager.Instance == null)
+        {
+            Debug.LogError("플레이어 인스턴스 없음");
+            return;
+        }
+        PlayerManager.Instance.OnPlayerStatusChanged += UpdateStatusUI;
+        UpdateStatusUI();
+    }
+    private void OnDestroy()
+    {
+        // 오브젝트가 파괴될 때 이벤트 등록 해제 (선택 사항이지만 안전함)
+        if (PlayerManager.Instance != null)
+        {
+            PlayerManager.Instance.OnPlayerStatusChanged -= UpdateStatusUI;
+        }
+    }
+    private void UpdateStatusUI()
+    {
+        int totalAtk = PlayerManager.Instance.TotalAttack();
+        int totalDef = PlayerManager.Instance.TotalDefence();
+
+        if(attackText != null)
+        {
+            attackText.text = $"{totalAtk}";
+        }
+        if(defenceText != null)
+        {
+            defenceText.text = $"{totalDef}";
+        }
     }
 }
